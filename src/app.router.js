830f6fe86626d034d759948a2b1b8848824ee725
tala dowiekat <taladweikat2017@gmail.com>
2024-05-04 10:55:45 +0300

    add CORS to allow client-side interaction with external servers

--- src/app.router.js
+++ src/app.router.js
@@ -1,8 +1,11 @@
 import connectDB from '../DB/connection.js';
 import categoriesRouter from './modules/category/category.router.js';
-import productRouter from './modules/product/product.router.js';
+import  productRouter from './modules/product/product.router.js';
+import cors from 'cors';
+
 const initApp = (app,express)=>{
     connectDB()
+    app.use(cors())
     app.use(express.json());
     app.get('/', (req,res)=>{
         return res.status(200).json({message:"Success"})
