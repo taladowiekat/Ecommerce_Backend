5eda552d7325bac6c1604c0e460e93741415218c
tala dowiekat <taladweikat2017@gmail.com>
2024-05-07 13:01:29 +0300

    add auth middleware to  create , update router

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import * as controller from './category.controller.js'
 import fileUpload, { fileType } from '../../utls/multer.js';
+import { auth } from '../../middleware/auth.middleware.js';
 const router = Router();
 
-router.post('/', fileUpload(fileType.image).single('image'),controller.create)
-
+router.post('/', auth() ,fileUpload(fileType.image).single('image'), controller.create);
+router.get('/',controller.getAll);
+router.get('/active',controller.getActive);
+router.get('/:id',controller.getDetails);
+router.patch('/:id',auth(),fileUpload(fileType.image).single('image'),controller.update);
+router.delete('/:id',auth(),controller.destroy)
 export default router;
