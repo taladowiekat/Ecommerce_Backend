509e3bb05eda6c43284db73fef523c3a7e321124
tala dowiekat <taladweikat2017@gmail.com>
2024-05-07 12:58:59 +0300

    make auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import userModel from "../../DB/model/user.model.js";
+
+
+export const auth = () => {
+    return async (req, res, next) => {
+        const { authorization } = req.headers;
+        if (!authorization?.startsWith(process.env.BEARERTOKEN)) {
+            return res.status(400).json({ message: "Invalid token" });
+        }
+
+        const token = authorization.split(process.env.BEARERTOKEN)[1];
+        const decoded = jwt.verify(token, process.env.LOGINSIG);
+        if (!decoded) {
+            return res.status(400).json({ message: "Invalid token" });
+        }
+
+        const user = await userModel.findById(decoded.id).select("userName");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        req.user = user;
+        next();
+    }
+}
