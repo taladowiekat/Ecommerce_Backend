6e3043d69b872738f4cc16b7124d1e7e4997c7ef
tala dowiekat <taladweikat2017@gmail.com>
2024-07-16 22:41:25 +0300

    edit array function using

--- src/modules/product/product.validation.js
+++ src/modules/product/product.validation.js
@@ -7,21 +7,9 @@ export const createCategorySchema = Joi.object({
     price: Joi.number().min(1).required(),
     discount: Joi.number().min(0).default(0),
 
-
-    mainImage: Joi.array.items({
-        filedname: Joi.string().required(),
-        originalname: Joi.string().required(),
-        encoding: Joi.string().required(),
-        mimeType: Joi.string().valid('image/png', 'image/jpeg', 'image/webp').required(),
-        destination: Joi.string().required(),
-        filename: Joi.string().required(),
-        path: Joi.string().required(),
-        size: Joi.number().max(5000000).required()
-    }).required(),
-
-    subImages: Joi.array.items(
+    mainImage: Joi.array().items(
         Joi.object({
-            filedname: Joi.string().required(),
+            fieldname: Joi.string().required(),
             originalname: Joi.string().required(),
             encoding: Joi.string().required(),
             mimeType: Joi.string().valid('image/png', 'image/jpeg', 'image/webp').required(),
@@ -29,11 +17,23 @@ export const createCategorySchema = Joi.object({
             filename: Joi.string().required(),
             path: Joi.string().required(),
             size: Joi.number().max(5000000).required()
-        })).max(5).optional(),
-        sizes: Joi.array().items(Joi.string().valid('s', 'm', 'lg', 'xl')).optional(),
-        categoryId:Joi.string().hex().length(24),
-        subcategoryId:Joi.string().hex().length(24),
-})
+        })
+    ).required(),
 
-
-
+    subImages: Joi.array().items(
+        Joi.object({
+            fieldname: Joi.string().required(),
+            originalname: Joi.string().required(),
+            encoding: Joi.string().required(),
+            mimeType: Joi.string().valid('image/png', 'image/jpeg', 'image/webp').required(),
+            destination: Joi.string().required(),
+            filename: Joi.string().required(),
+            path: Joi.string().required(),
+            size: Joi.number().max(5000000).required()
+        })
+    ).max(5).optional(),
+    
+    sizes: Joi.array().items(Joi.string().valid('s', 'm', 'lg', 'xl')).optional(),
+    categoryId: Joi.string().hex().length(24),
+    subcategoryId: Joi.string().hex().length(24),
+});
