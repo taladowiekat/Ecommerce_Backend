090a11deac1fc12c3d6eaf1da126ad8aee1ae399
tala dowiekat <taladweikat2017@gmail.com>
2024-07-16 21:17:22 +0300

    add check email if in the email in the database

---
+++ src/middleware/checkEmail.js
@@ -0,0 +1,14 @@
+import userModel from "../../DB/model/user.model.js";
+import { AppError } from "../utls/AppError.js";
+
+export const checkEmail = async(req, res,next)=>{
+    const {email} =req.body;
+
+    const user = await userModel.findOne({email: email});
+
+    if(user){
+        return next(new AppError (`email already exists` , 409))
+    }
+
+    next();
+}
