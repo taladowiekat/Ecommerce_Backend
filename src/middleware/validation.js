415e09bed288763c6bf7def9974fa11d9db54209
tala dowiekat <taladweikat2017@gmail.com>
2024-07-16 21:18:53 +0300

    add validation middleware

---
+++ src/middleware/validation.js
@@ -0,0 +1,21 @@
+export const validation = (schema) =>{
+    return (req , res , next)=>{
+    const errorMessage =[]
+    let filterData = {}
+    if(req.file){
+        filterData = {image:req.file,...req.body , ...req.params ,...req.query}
+    }else if(req.files){
+        filterData = {...req.files,...req.body , ...req.params ,...req.query}
+    }
+    const {error} =schema.validate( filterData, {abortEarly:false});
+    if (error) {
+        error.details.forEach(err => {
+            const key = err.context.key
+            errorMessage.push({[key]:err.message});
+        });
+        return res.status(400).json({message: "validation error", error: errorMessage});
+    }
+    next();
+    }
+
+}
