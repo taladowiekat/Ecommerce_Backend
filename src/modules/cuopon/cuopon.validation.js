452adf64f4bbd2d163b1f7312f1979e703163739
tala dowiekat <taladweikat2017@gmail.com>
2024-07-16 22:45:28 +0300

    fix mistake for using joi.number()

--- src/modules/cuopon/cuopon.validation.js
+++ src/modules/cuopon/cuopon.validation.js
@@ -2,10 +2,9 @@ import Joi from "joi" ;
 
 export const createCuoponSchema = Joi.object({
     name : Joi.string().min(3).required(),
-    amount: Joi.integer().min(100).max(50).optional(),
+    amount: Joi.number().integer().min(50).max(100).optional(), 
     expireDate: Joi.date().greater('now')
 })
-
 
 export const getDetailsSchema = Joi.object({
     id:Joi.string().hex().length(24),
@@ -16,7 +15,7 @@ export const updateCategorySchema = Joi.object({
     name:Joi.string().min(3),
     status: Joi.string().valid("Active", "NotActive"),
     image: Joi.object({
-        filedname: Joi.string().required(),
+        fieldname: Joi.string().required(),
         originalname: Joi.string().required(),
         encoding: Joi.string().required(),
         mimeType: Joi.string().valid('image/png', 'image/jpeg', 'image/webp').required(),
