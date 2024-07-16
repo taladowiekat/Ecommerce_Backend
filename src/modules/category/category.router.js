0282eb638f173266a63bb70c3d8c61083c582afc
tala dowiekat <taladweikat2017@gmail.com>
2024-07-16 21:19:24 +0300

    edite category module

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -3,18 +3,29 @@ import * as controller from './category.controller.js'
 import fileUpload, { fileType } from '../../utls/multer.js';
 import { auth } from '../../middleware/auth.middleware.js';
 import subCategoriesRouter from '../subCategory/subCategory.router.js';
+import {validation} from '../../middleware/validation.js'
+import * as schema from './category.validation.js'
+import { asyncHandler } from '../../utls/catchError.js';
+
 const router = Router();
 
-const roles = {
-    Admin : 'Admin',
-    User : 'User'
-}
 //id==>category
 router.use('/:id/subCategory',subCategoriesRouter);
-router.post('/', auth(['Admin']) ,fileUpload(fileType.image).single('image'), controller.create);
-router.get('/',controller.getAll);
-router.get('/active',controller.getActive);
-router.get('/:id',controller.getDetails);
-router.patch('/:id',auth(),fileUpload(fileType.image).single('image'),controller.update);
-router.delete('/:id',auth(),controller.destroy)
+
+
+router.post('/', fileUpload(fileType.image).single('image'),validation(schema.createCategorySchema),auth(['Admin']) , asyncHandler(controller.create));
+
+
+router.get('/',asyncHandler(controller.getAll));
+
+
+router.get('/active',asyncHandler(controller.getActive));
+
+router.get('/:id',validation(schema.getDetailsSchema),auth(['User']),asyncHandler(controller.getDetails));
+
+
+router.patch('/:id',fileUpload(fileType.image).single('image'),validation(schema.updateCategorySchema),auth(['User']),asyncHandler(controller.update));
+
+
+router.delete('/:id',validation(schema.deleteCategorySchema),auth(['User']),asyncHandler(controller.destroy))
 export default router;
